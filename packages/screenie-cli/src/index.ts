c898318b43266b7aa78a8c0192fd1f825847eb45
Dustin Schau <dustinschau@gmail.com>
2017-10-27 22:02:11 -0500

    add optional args for height/width

--- packages/screenie-cli/src/index.ts
+++ packages/screenie-cli/src/index.ts
@@ -7,11 +7,11 @@ import * as path from 'path';
 const { version } = require(path.join(__dirname, '../package.json'));
 
 program.version(version)
-  .option('-u --url <url>', 'The URL to scrape for screenshots')
+  .option('-u, --url <url>', 'The URL to scrape for screenshots')
   .option('-f, --folder <folder>', 'The folder to save the resulting screenshots (does not have to exist, will be created)')
-  .option('-d, --delay <delay>', 'A delay when taking each screenshot', parseInt)
-  .option('-h, --height <height>', 'A height when taking each screenshot', parseInt)
-  .option('-d, --width <width>', 'A width when taking each screenshot', parseInt)
+  .option('-d, --delay [delay]', 'A delay when taking each screenshot', parseInt)
+  .option('-h, --height [height]', 'A height when taking each screenshot', parseInt)
+  .option('-w, --width [width]', 'A width when taking each screenshot', parseInt)
   .parse(process.argv);
 
 (async () => {
@@ -20,10 +20,12 @@ program.version(version)
     url,
     folder,
     delay,
-    viewport: {
-      height,
-      width
-    }
+    ...(height & width ? {
+      viewport: {
+        height,
+        width
+      }
+    } : {})
   })
     .then(() => {
       console.log(chalk.green.bold(`Screenshots saved to ${folder}`));
