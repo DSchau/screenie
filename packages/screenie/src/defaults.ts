875fdf17bdf7e06e846eeb8fbbb8fe95eb19349b
Dustin Schau <dustinschau@gmail.com>
2017-10-27 22:37:24 -0500

    add defaults; only add delay when taking screenshot

---
+++ packages/screenie/src/defaults.ts
@@ -0,0 +1,10 @@
+export const defaults = opts => ({
+  ...{
+    delay: 0,
+    viewport: {
+      height: 450,
+      width: 800
+    }
+  },
+  ...opts
+});
--- packages/screenie/src/screenie.ts
+++ packages/screenie/src/screenie.ts
@@ -2,46 +2,40 @@ import * as puppeteer from 'puppeteer';
 import * as path from 'path';
 import * as fs from 'fs-extra';
 
+import { defaults } from './defaults';
+
 import { ScreenieOptions } from './interfaces';
 
 const takeScreenshot = (page, file) => page.screenshot({ path: file });
 
-const defaults = {
-  viewport: {
-    width: 800,
-    height: 450
-  }
-};
-
 export async function screenie(options: ScreenieOptions) {
+  const opts = defaults(options);
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
-  await fs.mkdirp(options.folder);
+  await fs.mkdirp(opts.folder);
 
-  await page.goto(options.url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
+  await page.goto(opts.url, { waitUntil: ['domcontentloaded', 'networkidle0'] });
   await page.setViewport({
-    ...defaults.viewport,
-    ...(options.viewport || {})
+    ...opts.viewport,
+    ...(opts.viewport || {})
   });
 
   let screenshots = [];
   let screenshot;
   let index = 0;
   while (true) {
-    const filePath = path.join(options.folder, `${index}.png`);
-    await page.waitFor(options.delay);
-    const newScreenshot = await takeScreenshot(page, path.join(options.folder, `${index}.png`));
+    const filePath = path.join(opts.folder, `${index}.png`);
+    await page.waitFor(opts.delay);
+    const newScreenshot = await takeScreenshot(page, path.join(opts.folder, `${index}.png`));
     if (screenshot && screenshot.equals(newScreenshot)) {
-      await fs.remove(path.join(options.folder, `${index}.png`));
+      await fs.remove(path.join(opts.folder, `${index}.png`));
       break;
     } else {
       screenshots.push(filePath);
     }
 
-    await page.waitFor(options.delay);
     await page.keyboard.down(' ');
-    await page.waitFor(options.delay);
 
     index += 1;
     screenshot = newScreenshot;
