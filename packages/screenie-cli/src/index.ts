3baaf846844036aa23534211d774c08cb61db038
Dustin Schau <dustinschau@gmail.com>
2017-10-29 19:11:27 -0500

    chore: bump versions

--- packages/screenie-cli/src/index.ts
+++ packages/screenie-cli/src/index.ts
@@ -10,7 +10,6 @@ program.version(version)
   .option('-u, --url <url>', 'The URL to scrape for screenshots')
   .option('-a, --adapter <adapter>', 'The adapter to run for screenshots')
   .option('-f, --folder <folder>', 'The folder to save the resulting screenshots (does not have to exist, will be created)')
-  .option('-f, --folder <folder>', 'The folder to save the resulting screenshots (does not have to exist, will be created)')
   .option('-p, --prepend [prepend]', 'Prepend each screenshot with a unique number', parseInt)
   .option('-d, --delay [delay]', 'A delay when taking each screenshot', parseInt)
   .option('-h, --height [height]', 'A height when taking each screenshot', parseInt)
@@ -18,21 +17,20 @@ program.version(version)
   .parse(process.argv);
 
 (async () => {
-  const { url, folder, delay, prepend, height, width } = program;
+  const { url, folder, delay, prepend, height, width, ...rest } = program;
   await screenie({
     url,
     folder,
     prependNumber: prepend || false,
     ...(delay ? { delay } : {}),
-    ...(height & width ? {
-      viewport: {
+    ...(height && width ? {
         height,
         width
-      }
-    } : {})
+    } : {}),
+    ...rest
   })
     .then(screenshots => {
       const len = screenshots.length;
-      console.log(chalk.green.bold(`(${len}) Screenshot${len === 1 ? '' : 's'} saved to ${folder}`));
+      console.log(chalk.green.bold(`(${len}) screenshot${len === 1 ? '' : 's'} saved to ${folder}`));
     });
 })();
